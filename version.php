68b5cb0fcc2a84814ae2f0ac018e24b9f43cccda
tunyesz@gmail.com <tunyesz@gmail.com>
2022-12-07 18:10:54 +0100

    Bump version

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_gamoteca';
 $plugin->release = '1.0.0';
-$plugin->version = 2022111500;
+$plugin->version = 2022120700;
 $plugin->requires = 2017051500;
 $plugin->maturity = MATURITY_STABLE;
