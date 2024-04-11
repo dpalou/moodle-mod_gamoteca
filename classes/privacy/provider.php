8d46c2d3dd357ebb7a89c069a53e529ddcdfac9c
tunyesz@gmail.com <tunyesz@gmail.com>
2024-04-11 16:42:48 +0200

    Fix #77: Privacy API implementation

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -19,11 +19,12 @@
  *
  * @package     mod_gamoteca
  * @category    privacy
- * @copyright   2022 Peter Varga <peter@gamoteca.com>
+ * @copyright   2022 Gamoteca <info@gamoteca.com>
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 namespace mod_gamoteca\privacy;
+use core_privacy\local\request\writer;
 use core_privacy\local\metadata\collection;
 use core_privacy\local\request\contextlist;
 use core_privacy\local\request\approved_contextlist;
@@ -72,7 +73,23 @@ class provider implements
      * @return contextlist The contextlist containing the list of contexts used in this plugin.
      */
     public static function get_contexts_for_userid(int $userid) : contextlist {
-        return new contextlist();
+        $sql = "SELECT c.id
+                  FROM {context} c
+            INNER JOIN {course_modules} cm ON cm.id = c.instanceid AND c.contextlevel = :contextlevel
+            INNER JOIN {modules} m ON m.id = cm.module AND m.name = :modname
+            INNER JOIN {gamoteca} g ON g.id = cm.instance
+            INNER JOIN {gamoteca_data} gd ON gd.id = cm.instance
+                 WHERE g.userid = :userid";
+
+        $params = [
+            'modname' => 'gamoteca',
+            'contextlevel' => CONTEXT_MODULE,
+            'userid' => $userid,
+        ];
+        $contextlist = new contextlist();
+        $contextlist->add_from_sql($sql, $params);
+
+        return $contextlist;
     }
 
     /**
@@ -81,6 +98,26 @@ class provider implements
      * @param userlist $userlist The userlist containing the list of users who have data in this context/plugin combination.
      */
     public static function get_users_in_context(userlist $userlist) {
+        $context = $userlist->get_context();
+
+        if (!$context instanceof \context_module) {
+            return;
+        }
+
+        // Fetch all the gamoteca data instance.
+        $sql = "SELECT gd.userid
+                  FROM {course_modules} cm
+                  JOIN {modules} m ON m.id = cm.module AND m.name = :modname
+                  JOIN {gamoteca} g ON g.id = cm.instance
+                  JOIN {gamoteca_data} gd ON gd.id = cm.instance
+                 WHERE cm.id = :cmid";
+
+        $params = [
+            'cmid' => $context->instanceid,
+            'modname' => 'gamoteca',
+        ];
+
+        $userlist->add_from_sql('userid', $sql, $params);
     }
 
     /**
@@ -89,6 +126,92 @@ class provider implements
      * @param approved_contextlist $contextlist The approved contexts to export information for.
      */
     public static function export_user_data(approved_contextlist $contextlist) {
+        global $DB;
+
+        if (empty($contextlist->count())) {
+            return;
+        }
+
+        $user = $contextlist->get_user();
+
+        list($contextsql, $contextparams) = $DB->get_in_or_equal($contextlist->get_contextids(), SQL_PARAMS_NAMED);
+
+        $sql = "SELECT cm.id AS cmid,
+                       gd.gameid as gameid,
+                       gd.score as score,
+                       gd.status as gamestatus,
+                       gd.timespent as timespent,
+                       gd.timecreated as timecreated,
+                       gd.timemodified as timemodified
+                  FROM {context} c
+            INNER JOIN {course_modules} cm ON cm.id = c.instanceid AND c.contextlevel = :contextlevel
+            INNER JOIN {modules} m ON m.id = cm.module AND m.name = :modname
+            INNER JOIN {gamoteca} g ON g.id = cm.instance
+            INNER JOIN {gamoteca_data} gd ON gd.gameid = g.id
+                 WHERE c.id {$contextsql}
+                       AND gd.userid = :userid
+              ORDER BY cm.id";
+
+        $params = ['modname' => 'gamoteca', 'contextlevel' => CONTEXT_MODULE, 'userid' => $user->id] + $contextparams;
+
+        // Reference to the gamoteca activity seen in the last iteration of the loop. By comparing this with the current record, and
+        // because we know the results are ordered, we know when we've moved to the data for a new gamoteca activity and therefore
+        // when we can export the complete data for the last activity.
+        $lastcmid = null;
+
+        $gamotecadatas = $DB->get_recordset_sql($sql, $params);
+
+        foreach ($gamotecadatas as $gamotecadata) {
+            // If we've moved to a new completion record, then write the last completion record data
+            // and re-init the completion record data array.
+            if ($lastcmid != $gamotecadata->cmid) {
+                if (!empty($gamotecaExportData)) {
+                    $context = \context_module::instance($lastcmid);
+                    self::export_gamoteca_data_for_user($gamotecaExportData, $context, $user);
+                }
+                $gamotecaExportData = [
+                    'score' => [],
+                    'status' => [],
+                    'timespent' => [],
+                    'timecreated' => [],
+                    'timemodified' => [],
+                ];
+            }
+            $gamotecaExportData['score'][] = $gamotecadata->score;
+            $gamotecaExportData['status'][] = $gamotecadata->gamestatus;
+            $gamotecaExportData['timespent'][] = $gamotecadata->timespent;
+            $gamotecaExportData['timecreated'][] = \core_privacy\local\request\transform::datetime($gamotecadata->timecreated);
+            $gamotecaExportData['timemodified'][] = \core_privacy\local\request\transform::datetime($gamotecadata->timemodified);
+
+            $lastcmid = $gamotecadata->cmid;
+        }
+
+        $gamotecadatas->close();
+
+        // The data for the last activity won't have been written yet, so make sure to write it now!
+        if (!empty($gamotecaExportData)) {
+            $context = \context_module::instance($lastcmid);
+            self::export_gamoteca_data_for_user($gamotecaExportData, $context, $user);
+        }
+    }
+
+    /**
+     * Export the supplied personal data for a single gamoteca game activity, along with any generic data or area files.
+     *
+     * @param array $gamotecaExportData the personal data to export for the gamoteca game.
+     * @param \context_module $context the context of the gamoteca game.
+     * @param \stdClass $user the user record
+     */
+    protected static function export_gamoteca_data_for_user(array $gamotecaExportData, \context_module $context, \stdClass $user) {
+        // Fetch the generic module data for the gamoteca game.
+        $contextdata = \core_privacy\local\request\helper::get_context_data($context, $user);
+
+        // Merge with game data and write it.
+        $contextdata = (object)array_merge((array)$contextdata, $gamotecaExportData);
+        writer::with_context($context)->export_data([], $contextdata);
+
+        // Write generic module intro files.
+        helper::export_context_files($context, $user);
     }
 
     /**
@@ -97,6 +220,15 @@ class provider implements
      * @param \context $context A user context.
      */
     public static function delete_data_for_all_users_in_context(\context $context) {
+        global $DB;
+
+        if (!$context instanceof \context_module) {
+            return;
+        }
+
+        if ($cm = get_coursemodule_from_id('gamoteca', $context->instanceid)) {
+            $DB->delete_records('gamoteca_data', ['gameid' => $cm->instance]);
+        }
     }
 
     /**
@@ -105,6 +237,24 @@ class provider implements
      * @param approved_userlist $userlist The approved context and user information to delete information for.
      */
     public static function delete_data_for_users(approved_userlist $userlist) {
+        global $DB;
+
+        if (empty($contextlist->count())) {
+            return;
+        }
+
+        $userid = $contextlist->get_user()->id;
+        foreach ($contextlist->get_contexts() as $context) {
+
+            if (!$context instanceof \context_module) {
+                continue;
+            }
+            $instanceid = $DB->get_field('course_modules', 'instance', ['id' => $context->instanceid]);
+            if (!$instanceid) {
+                continue;
+            }
+            $DB->delete_records('gamoteca_data', ['gameid' => $instanceid, 'userid' => $userid]);
+        }
     }
 
     /**
@@ -113,5 +263,26 @@ class provider implements
      * @param approved_contextlist $contextlist The approved contexts and user information to delete information for.
      */
     public static function delete_data_for_user(approved_contextlist $contextlist) {
+        global $DB;
+
+        $context = $userlist->get_context();
+
+        if (!$context instanceof \context_module) {
+            return;
+        }
+
+        $cm = get_coursemodule_from_id('gamoteca', $context->instanceid);
+
+        if (!$cm) {
+            // Only gamoteca module will be handled.
+            return;
+        }
+
+        $userids = $userlist->get_userids();
+        list($usersql, $userparams) = $DB->get_in_or_equal($userids, SQL_PARAMS_NAMED);
+
+        $select = "gameid = :gameid AND userid $usersql";
+        $params = ['gameid' => $cm->instance] + $userparams;
+        $DB->delete_records_select('gamoteca_data', $select, $params);
     }
 }
